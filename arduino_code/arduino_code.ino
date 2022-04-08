a6788a36f565c0dd883b4cc33e6c9fb1fa6c1491
Simon Paulsson <simon@paulsson.one>
2022-04-08 12:05:38 +0200

    arduino code

---
+++ arduino_code/arduino_code.ino
@@ -0,0 +1,83 @@
+#define COILS 2
+#define SEPARATOR ','
+#define END '\n'
+
+int fire_pins[COILS] = {10, 11};
+int sensor_pins[COILS] = {20, 21};
+int voltage_pins[COILS] = {A0, A1};
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // Setup all the pins
+  for (int i = 0; i < COILS; i++) {
+    pinMode(fire_pins[i], OUTPUT);
+    pinMode(sensor_pins[i], INPUT);
+    pinMode(voltage_pins[i], INPUT);
+
+    digitalWrite(fire_pins[i], LOW);
+  }
+
+  // Begin serial communication
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  while (Serial.available() == 0) {
+    // Wait for command from the RP
+  }
+
+  String command = Serial.readStringUntil(END);
+
+  if (command == "FIRE") {
+    Fire();
+  }
+  else if (command == "VOLTAGE") {
+    ReadVoltage();
+  }
+  else {
+    Serial.print("UNKNOWN COMMAND...");
+    Serial.print(END);
+  }
+}
+
+void SendData(unsigned long data[], int size_of_data) {
+  String content = "";
+  for (int i = 0; i < size_of_data; i++) {
+    if (i > 0) {
+      content += SEPARATOR;
+    }
+    content += data[i];
+  }
+  content += END;
+  int test = Serial.print(content);
+}
+
+void Fire() {
+  unsigned long blocking_times[COILS];
+
+  // Fire the coils and read there velocity (blocking time)
+  for (int i = 0; i < COILS; i++) {
+    digitalWrite(fire_pins[i], HIGH);
+    blocking_times[i] = pulseIn(sensor_pins[i], LOW, 10000);
+  }
+
+  // Reset all the pins
+  for (int i = 0; i < COILS; i++) {
+    digitalWrite(fire_pins[i], LOW);
+  }
+
+  SendData(blocking_times, COILS);
+}
+
+void ReadVoltage() {
+  unsigned long voltages[COILS];
+
+  for (int i = 0; i < COILS; i++) {
+    voltages[i] = (unsigned long) analogRead(voltage_pins[i]);
+  }
+
+  SendData(voltages, COILS);
+}
